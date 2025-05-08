e0ebc424d5dac32ea9c2ded0595999c883949097
config <101471590+general132@users.noreply.github.com>
2025-05-08 20:32:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,55 +6,25 @@ const io = require('socket.io')(http);
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static('public'));
 
-let users = {}; // Armazenar dados dos usuários
-
+// Evento de conexão
 io.on('connection', (socket) => {
   console.log('Um usuário entrou');
-
-  // Ouvir evento de "join chat" com informações de usuário
-  socket.on('join chat', (userInfo) => {
-    users[socket.id] = userInfo; // Armazenar nome e cor do usuário
-    console.log(`Novo usuário: ${userInfo.username} com cor: ${userInfo.color}`);
-
-    // Notificar todos os usuários sobre a entrada
-    io.emit('chat message', {
-      type: 'system',
-      content: `${userInfo.username} entrou no chat.`,
-      username: 'Sistema',
-      color: '#000000'
-    });
-  });
 
   // Ouvir o evento 'chat message' para mensagens de texto ou arquivos
   socket.on('chat message', (msg) => {
     console.log('Mensagem recebida: ', msg);
 
-    // Verificar se o usuário existe antes de enviar a mensagem
-    if (users[socket.id]) {
-      // Emitir a mensagem para todos os outros clientes
-      io.emit('chat message', msg);
-    }
+    // Emitir a mensagem para todos os outros clientes sem a parte do sistema
+    io.emit('chat message', msg);
   });
 
   // Quando um usuário desconectar
   socket.on('disconnect', () => {
     console.log('Um usuário saiu');
-    
-    // Enviar uma mensagem para todos quando um usuário sai
-    if (users[socket.id]) {
-      const username = users[socket.id].username;
-      io.emit('chat message', {
-        type: 'system',
-        content: `${username} saiu do chat.`,
-        username: 'Sistema',
-        color: '#000000'
-      });
-      delete users[socket.id]; // Remover o usuário da lista
-    }
   });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${3000}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
